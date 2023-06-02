763c06f543a135e33a24cb4b8a6e8e8f734bb59a
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-06-01 20:49:18 -0600

    added updated sequelized models for future use

--- models/Activism.js
+++ models/Activism.js
@@ -1,25 +1,58 @@
-const Organization = require("./Organization");
-
 const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 //all activism organizations should be derived from this class
-class Activism extends Organization {
-  constructor(
-    name,
-    location,
-    hours,
-    description,
-    reachUs,
-    additionalInfo,
-    cause,
-    homepage
-  ) {
-    super(name, location, hours, description, reachUs, additionalInfo);
-    this.cause = cause;
-    this.homepage = homepage;
+class Activism extends Model {}
+
+Activism.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    cause: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    hours: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contact: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    additional_info: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "activism",
   }
-}
+);
 
 module.exports = Activism;
