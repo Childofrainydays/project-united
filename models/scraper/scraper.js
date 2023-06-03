c99e1b0f25b86f419755ff16788112ae5ecfcfd9
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-06-03 17:04:50 -0600

    First try version! proofreading/testing/functionality verification appreciated

--- models/scraper/scraper.js
+++ models/scraper/scraper.js
@@ -1,13 +1,37 @@
+// Dependencies
 const axios = require("axios");
+const cheerio = require("cheerio");
 
 axios
   .get("http://app.scrapingbee.com/api/v1", {
     params: {
-      api_key: "",
+      api_key: "0BTE3F88UR9Y2VPX081XHVPTP29JONJAW9J6UXU7SKGLXH10VU2H98L7GEYEQWTX120029G8S9VG9X8O",
+      // The target webpage :
       url: "https://www.slugmag.com/category/community/",
     },
   })
+  // Retrieve the page
   .then(function (response) {
     const News = response;
     console.log(News.data);
+
+    // Load the response data into Cheerio  !! Important
+    const $ = cheerio.load(News.data);
+
+    // Use Cheerio selectors to extract specific data
+    // Need to find the correct selectors ( known : <a> for article links)
+    // Double check capitalization consistency for variables
+    const newsTitles = $("card-title a").text();
+    // Can be made more specific by adding a class or id to the <a> tag
+    const newsLinks = $("a").attr("href");
+    const newsSummaries = $("card-text p").text();
+
+    // Output the data in the terminal
+    console.log(newsTitles);
+    console.log(newsLinks);
+    console.log(newsSummaries);
+  })
+  // Catch and log errors
+  .catch(function (error) {
+    console.error(error);
   });
