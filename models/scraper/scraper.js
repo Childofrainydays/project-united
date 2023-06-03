c165f4e9377343e4f8335383e5e250531732cb2b
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-06-02 21:42:55 -0600

    adding scraping code again

--- models/scraper/scraper.js
+++ models/scraper/scraper.js
@@ -3,8 +3,7 @@ const axios = require("axios");
 axios
   .get("http://app.scrapingbee.com/api/v1", {
     params: {
-      api_key:
-        "TIO7SISJT4XBKE803L8PJ7YLBOGJXCSC7SF2WPNTPD7MR94WQAFQ13GU897Z0WZALXQH1ASCGB5GIH6L",
+      api_key: "",
       url: "https://www.slugmag.com/category/community/",
     },
   })
