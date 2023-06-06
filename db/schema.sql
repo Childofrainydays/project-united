7c4e76d5f32254c15d48e80593c4f85c5f76c687
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-06-05 19:13:03 -0600

    added standardization for the database formatting

--- db/schema.sql
+++ db/schema.sql
@@ -2,24 +2,27 @@ DROP DATABASE IF EXISTS united_db;
 
 CREATE DATABASE united_db;
 
+USE united_db;
+
 CREATE TABLE activism(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    a_name = VARCHAR(100) NOT NULL,
-    a_cause = VARCHAR(150),
-    a_location = VARCHAR(150) NOT NULL,
-    a_hours = VARCHAR(100),
-    a_description = VARCHAR(550) NOT NULL,
-    a_contact = VARCHAR(150),
-    a_additional_info = VARCHAR(150),
-    date_created =  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    a_name VARCHAR(100) NOT NULL,
+    a_cause VARCHAR(150) NOT NULL,
+    a_location VARCHAR(150) NULL,
+    a_hours VARCHAR(100) NULL,
+    a_description VARCHAR(550) NOT NULL,
+    a_contact VARCHAR(150) NULL,
+    a_additional_info VARCHAR(150) NULL,
+    date_created  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-CREATE TABLE friendlyspaces(
+
+CREATE TABLE friendlyspaces (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    fs_name = VARCHAR(100) NOT NULL,
-    fs_location = VARCHAR(150),
-    fs_hours = VARCHAR(100),
-    fs_description = VARCHAR(550) NOT NULL,
-    fs_contact = VARCHAR(150),
-    fs_additional_info = VARCHAR(150),
-    fs_friendly_rating = INT(5) NOT NULL,
+    fs_name VARCHAR(100) NOT NULL,
+    fs_location VARCHAR(150) NOT NULL,
+    fs_hours VARCHAR(100) NOT NULL,
+    fs_description VARCHAR(550) NOT NULL,
+    fs_contact VARCHAR(150) NULL,
+    fs_additional_info VARCHAR(150) NULL,
+    fs_friendly_rating INT NOT NULL
 );
