6f8ed7dac8edad9796a71da6789f863bd41d8f68
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-06-05 18:37:01 -0600

    Check out these scrapers

--- models/scraper/kslscraper.js
+++ models/scraper/kslscraper.js
@@ -2,7 +2,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-// Initial basic set-up for optional second site to scrape (in case)
 axios
   .get("http://app.scrapingbee.com/api/v1", {
     params: {
@@ -11,3 +10,46 @@ axios
       url: "https://www.ksl.com/news/stream/multicultural-utah",
     },
   })
+
+ // Retrieve the page
+ .then(function (response) {
+  const News = response;
+  //console.log(News.data);
+
+  // Load the response data into Cheerio !! Important
+  const $ = cheerio.load(News.data);
+
+  // Create an empty array to save the data
+  const articles = [];
+
+  // use Cheerio selectors to extract specific data
+  // Iterate over each .card element to get specific data (title, link, summary)
+  $(".queue_story").each(function () {
+       
+      const article = [];
+
+      // $(this) for each .card element
+      //.trim() to remove whitespace
+      article.title = $(this).find(".headline h2").text().trim();
+      article.link = $(this).find(".headline h2 a").attr("href");
+      article.summary = $(this).find(".headline h5").text().trim();
+      
+      // !! Push the article data into the articles array
+      articles.push(article);
+  });
+
+  // Output the extracted data /for each/ article 
+  articles.forEach((article) => { 
+    console.log("Title:", article.title);
+    console.log("Link:", article.link);
+    console.log("Summary:", article.summary);
+    // For readability in terminal
+    console.log("-------------------------");
+  });
+})
+
+// Catch and log errors
+// !! Edit for improved error handling when not 3 AM on a Saturday
+.catch(function (error) {
+  console.error(error);
+});
