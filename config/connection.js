318789e4732fa3c893e24c955236752096007d61
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-06-06 19:47:05 -0600

    secondary heroku push

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    "cyu680aj3dopzf69",
-    "ghnbd4q0e4qhzg91",
-    "jnr529nag7t327e1",
+    "opuph324pl23iqj4",
+    "ngxmpkd352prknur",
+    "kv2n01cppbfgmiok",
     {
       host: "co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       dialect: "mysql",
